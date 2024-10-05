82fe70af9f6724a5ffd65455201a763f59bbdcf0
IagoACPereira <iago0307@gmail.com>
2024-10-05 16:00:18 -0300

    Inicio do projeto Spring

---
+++ src/main/java/br/com/alura/screenmatch/ScreenmatchApplication.java
@@ -0,0 +1,27 @@
+package br.com.alura.screenmatch;
+
+import br.com.alura.screenmatch.models.DadosSeries;
+import br.com.alura.screenmatch.service.ConsumoApi;
+import br.com.alura.screenmatch.service.ConverteDados;
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ScreenmatchApplication implements CommandLineRunner {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ScreenmatchApplication.class, args);
+	}
+
+	@Override
+	public void run(String... args) throws Exception {
+		ConsumoApi consumoApi = new ConsumoApi();
+		var json = consumoApi.obterDados("https://www.omdbapi.com/?t=gilmore+girls&apikey=af3e3d3c");
+		System.out.println(json);
+
+		ConverteDados conversor = new ConverteDados();
+		DadosSeries dados = conversor.obterDados(json, DadosSeries.class);
+		System.out.println(dados);
+	}
+}
---
+++ src/main/java/br/com/alura/screenmatch/models/DadosSeries.java
@@ -0,0 +1,13 @@
+package br.com.alura.screenmatch.models;
+
+import com.fasterxml.jackson.annotation.JsonAlias;
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+
+@JsonIgnoreProperties(ignoreUnknown = true)
+public record DadosSeries(
+        @JsonAlias("Title") String titulo,
+        @JsonAlias("totalSeasons") Integer totalTemporadas,
+        @JsonAlias("imdbRating") String avaliacao
+) {
+
+}
