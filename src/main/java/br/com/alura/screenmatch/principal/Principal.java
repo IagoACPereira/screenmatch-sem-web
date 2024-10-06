bd826cc1da2e12388c18cc3c1e3e8eeba0ffb3b9
IagoACPereira <iago0307@gmail.com>
2024-10-06 20:48:17 -0300

    feat: add: aula2: Trabalhando na coleção de dados

--- src/main/java/br/com/alura/screenmatch/principal/Principal.java
+++ src/main/java/br/com/alura/screenmatch/principal/Principal.java
@@ -1,5 +1,6 @@
 package br.com.alura.screenmatch.principal;
 
+import br.com.alura.screenmatch.models.DadosEpisodio;
 import br.com.alura.screenmatch.models.DadosSeries;
 import br.com.alura.screenmatch.models.DadosTemporada;
 import br.com.alura.screenmatch.service.ConsumoApi;
@@ -22,14 +23,23 @@ public class Principal {
         var json = consumo.obterDados(ENDERECO + nomeSerie.replace(" ", "+") + API_KEY);
         DadosSeries dados = conversor.obterDados(json, DadosSeries.class);
         System.out.println(dados);
+
         List<DadosTemporada> temporadas = new ArrayList<>();
-		for (int i = 1; i <= dados.totalTemporadas(); i++) {
+
+        for (int i = 1; i <= dados.totalTemporadas(); i++) {
 			json = consumo.obterDados(ENDERECO + nomeSerie.replace(" ", "+") + "&season=" + i + API_KEY);
 			DadosTemporada dadosTemporada = conversor.obterDados(json, DadosTemporada.class);
 			temporadas.add(dadosTemporada);
 		}
 		temporadas.forEach(System.out::println);
+
+//        for (int i = 0; i < dados.totalTemporadas(); i++) {
+//            List<DadosEpisodio> episodiosTemporada = temporadas.get(i).episodios();
+//            for (int j = 0; j < episodiosTemporada.size(); j++) {
+//                System.out.println(episodiosTemporada.get(j).titulo());
+//            }
+//        }
+
+        temporadas.forEach(t -> t.episodios().forEach(e -> System.out.println(e.titulo())));
     }
 }
-
-// gilmore+girls
